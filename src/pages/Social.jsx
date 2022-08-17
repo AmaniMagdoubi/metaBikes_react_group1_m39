832a9f0b3a2ca2fbc8ecb401e981aaf90c6e0073
Seb Amos <sebastian.amos8@gmail.com>
2022-08-17 16:11:17 +0100

    social updated

--- src/pages/Social.jsx
+++ src/pages/Social.jsx
@@ -1,9 +1,30 @@
 
 import React from 'react'
+import {useState, useEffect} from 'react';
 
-const Social = () => {
+const Social = ({user}) => {
+  const [pics, setPics]= useState([]);
+  const [display, setDisplay]= useState(false);
+
+
+  const fetchPics = async()=>{
+    const res = await fetch('https://picsum.photos/v2/list');
+    const pictures = await res.json();
+    setPics(pictures);console.log(pictures);
+  } 
+  useEffect(()=>{fetchPics();console.log(pics);}, [user])
   return (
-    <div>Social</div>
+    <div>
+    {user ? <div>
+    
+    <button onClick={(event)=>{setDisplay(!display)}}>images</button>
+    {display && pics.map((item, index)=>{
+      return (<div>
+        <h3>{item.author}</h3>
+        <img alt='pic' src={item.download_url}></img>
+      </div>)
+    })}</div> : <h1>not logged in</h1>}
+    </div>
   )
 }
 
