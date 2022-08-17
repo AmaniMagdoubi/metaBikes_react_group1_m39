0784786d94197090fc92f01812626034e153ebc4
AmaniMagdoubi <amanimagdoubi@gmail.com>
2022-08-17 12:32:28 +0100

    homepage JSX code

---
+++ src/components/6-home-about.js
@@ -0,0 +1,23 @@
+const Home = () => {
+    return(
+        <div>
+            <h1>META BIKES</h1>
+            <h2>Plan a trip with your friends online and meet offline!</h2> 
+            <h2>WELCOME TO META BIKES</h2> 
+            <p>Your all-in-one platform whether you are a professional or hobbyist biker. We're touring-friendly and here to help you every step of the way. Plan your eagerly-awaited adventure, join our community and sell, buy and share gear on our e-Bike store!</p>
+
+            <ul>
+                <li>Build friendships</li>
+                <li>Plan your journey with ease</li>
+                <li>Check the weather forcast</li>
+                <li>Buy and sell on our e-Bike store</li>
+            </ul>
+
+            <p>Take a Look!</p>
+            <p>Share with your friends!</p>
+            <p>images / design</p>
+        </div>
+    )
+}
+
+export default Home; 
