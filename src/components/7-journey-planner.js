3b8f5c12355e5b6c0459700d9da82796a861d10b
Shaé <Cshaetucker@gmail.com>
2022-08-17 12:19:34 +0100

    Styles setup

---
+++ src/components/7-journey-planner.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../index.css"; //Golbal styles
+import {GolbalWrapper} from "../styles/global.styles.js"; //Golbal wrapper style
+
+export const JourneyPlanner = () => {
+  return (
+    <GolbalWrapper>
+    </GolbalWrapper>
+  )
+}
