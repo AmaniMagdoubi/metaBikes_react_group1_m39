4e9acaf89d627279c60029c1ddebc2a19d93fd12
Seb Amos <sebastian.amos8@gmail.com>
2022-08-17 11:24:26 +0100

    basic crud functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from "react"
+import {useState, useEffect} from 'react';
+import SignupOrLogin from "./components/1-signup";
+import DeleteUser from "./components/15-delete-user";
+import UpdateUser from "./components/14-edit-user";
+import ListUsers from "./components/12-list-users";
+
 import "./index.css";
 
 const App = () => {
+  const [user, setUser]= useState('')
+  const [pics, setPics]= useState([]);
+  const [display, setDisplay]= useState(false);
+  const [allUsers, setUsers]= useState(['']);
+  const [del, setDel]= useState();
+  const [upd, setUpd]= useState();
+
+  const fetchPics = async()=>{
+    const res = await fetch('https://picsum.photos/v2/list');
+    const pictures = await res.json();
+    setPics(pictures);console.log(pictures);
+  } 
+  useEffect(()=>{fetchPics();console.log(pics);}, [user])
   return (
     <div>
-
+      <SignupOrLogin setter={setUser}></SignupOrLogin>
+      {user ? <div><h1>{user} logged in</h1>
+      <ListUsers setter={setUsers} /> {allUsers.map((allUsers)=>(
+        <ul>
+          <h2>{allUsers}</h2>
+        </ul>
+      ))} 
+      
+      <DeleteUser setter={setDel}>User deleted: {del}</DeleteUser>
+      <UpdateUser setter={setUpd}>User updated: {upd}</UpdateUser>
+      <button onClick={(event)=>{setDisplay(!display)}}>images</button>
+      {display && pics.map((item, index)=>{
+        return (<div>
+          <h3>{item.author}</h3>
+          <img alt='pic' src={item.download_url}></img>
+        </div>)
+      })}</div>: <h1>not logged in</h1>}
       <h1>Test heading 1</h1>
       <h2>Test heading 2</h2>
       <h3>Test heading 3</h3>
