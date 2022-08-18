1a545cd6fcb2f69ebeccc34fd5767409f902a072
PaulEnt <pauljamesentwistle@gmail.com>
2022-08-18 16:37:00 +0100

    added a map window from Google Map API

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from "react";
 import {useState, } from 'react';
 import "./index.css";
 import { JourneyPlanner } from "./components/7-journey-planner";
+import HomePage from "./pages/Home";
 import About from "./components/6-home-about";
 import Switch from "./components/17-switch";
-
 
 const App = () => {
 
   return (
     <div>
       <About />
-      <JourneyPlanner/>
+      <JourneyPlanner />
       
       
-      <Switch ></Switch>
+      <Switch></Switch>
 
-
+      <HomePage />
      
     </div>
   );
