5e97fc93dae6288ac41e75cb06da7032ba9ee6e2
PaulEnt <pauljamesentwistle@gmail.com>
2022-08-19 15:14:35 +0100

    added direction funtionality to the Google Map API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,138 @@
-import {useMemo} from 'react'
-import {GoogleMap, useLoadScript, Marker} from '@react-google-maps/api'
-//Google Maps API Key: AIzaSyDQ3RmCjCbjvPJTrwzu8HFah1y306uev3Q
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+} from '@chakra-ui/react'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
-const mapContainerStyle = {
-  width: '100vh',
-  height: '100vh',
-};
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from '@react-google-maps/api'
+import { useRef, useState } from 'react'
 
-const center = { lat: 48.8584, lng: 2.2945 };
+const center = { lat: 53.48368749923018, lng: -2.246122150819447 }
 
-export default function HomePage() {
-  const { isLoaded } = useLoadScript({
+function Homepage() {
+  const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ['places'],
   })
 
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />
-};
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
 
-function Map() {
-  return <GoogleMap
-    zoom={10}
-    center={center}
-    mapContainerStyle={mapContainerStyle}>
-  </GoogleMap>
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef()
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destiantionRef = useRef()
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  async function calculateRoute() {
+    if (originRef.current.value === '' || destiantionRef.current.value === '') {
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destiantionRef.current.value = ''
+  }
+
+  return (
+    <div>
+      <Box position='absolute' left={0} top={0} h='100vh' w='100vh'>
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={12}
+          mapContainerStyle={{ width: '100vh', height: '100vh' }}
+          onLoad={map => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
+      <Box
+        p={4}
+        borderRadius='lg'
+        m={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='1'
+      >
+        <HStack spacing={2} justifyContent='space-between'>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input type='text' placeholder='Origin' ref={originRef} />
+            </Autocomplete>
+          </Box>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input
+                type='text'
+                placeholder='Destination'
+                ref={destiantionRef}
+              />
+            </Autocomplete>
+          </Box>
+
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: {distance} </Text>
+          <Text>Duration: {duration} </Text>
+          <IconButton
+            aria-label='center back'
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => {
+              map.panTo(center)
+              map.setZoom(12)
+            }}
+          />
+        </HStack>
+      </Box>
+    </div>
+  )
 }
+
+export default Homepage
