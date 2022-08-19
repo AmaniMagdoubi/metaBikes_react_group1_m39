1fed01cb7e70197ad3cd924ba1eedcde5b8c20e3
PaulEnt <pauljamesentwistle@gmail.com>
2022-08-19 09:45:48 +0100

    reverted back to when the map API was showing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {useState, } from 'react';
 import "./index.css";
 import { JourneyPlanner } from "./components/7-journey-planner";
 import About from "./components/6-home-about";
