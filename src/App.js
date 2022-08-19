a3900972a0d7ea7a98f304db8f91efb310974a2c
Seb Amos <sebastian.amos8@gmail.com>
2022-08-19 11:26:36 +0100

    done some of it, still have to sort the bottom rendering, will do with an if/switch statement, have tried to include what goes where, though not sure if i missed some files, going to work on more over weekend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from "react";
 import {useState, } from 'react';
 import "./index.css";
-import { JourneyPlanner } from "./components/7-journey-planner";
+import JourneyPlanner from "./components/7-journey-planner";
 import HomePage from "./pages/Home";
 import About from "./components/6-home-about";
+
+import SignupOrLogin from "./components/1-signup";
+import Router from "./components/16-router";
 import Switch from "./components/17-switch";
 
-const App = () => {
+
+const App = ({user}) => {
 
   return (
-    <div>
-      <About />
-      <JourneyPlanner />
+    
       
-      
-      <Switch></Switch>
+      <div user={user}>
+        
+        <Switch></Switch>
+        <About></About>
+        {/* <LeftNav></LeftNav> */}
+        <JourneyPlanner></JourneyPlanner>
+        {/*  Footer  */}
+      </div>
 
-      <HomePage />
+      
      
-    </div>
+    
   );
 };
 
