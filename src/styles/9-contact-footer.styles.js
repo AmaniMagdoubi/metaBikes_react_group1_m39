2f85ea68177bdae89594eaaf737033587eb8c38f
PaulEnt <106552931+PaulEnt@users.noreply.github.com>
2022-08-19 10:05:33 +0100

    Revert "Update Styles"

--- src/styles/9-contact-footer.styles.js
+++ src/styles/9-contact-footer.styles.js
@@ -1,36 +1,11 @@
 import styled from "styled-components";
 import "../index.css";
 
-export const FooterWrapper = styled.div`
+export const InterestWrapper = styled.div`
 
-    background: var(--clr-text-light);
-    height: 10em;
-    width: 100%;
-    display: flex;
+    height: 100%;
+    display:flex;
     align-items: center;
-    justify-content: center;
 
-    .footer_wrapper{
-
-        width: 80%;
-        display: flex;
-        align-items: center;
-
-    }
-
-    .footer_text{
-
-        margin: auto;
-
-        p{
-            margin: 20px;
-        }
-
-    }
-
-    .footer_icons{
-
-    }
-
-
+    
 `
--- src/styles/global.styles.js
+++ src/styles/global.styles.js
@@ -30,30 +30,3 @@ export const Row = styled.div`
 
 
 `
-export const Column = styled.div`
-
-    display: flex;
-    flex-direction: column;
-
-
-`
-
-export const GolbalNavWrapper = styled.div`
-
-    position: fixed;
-    margin: auto;
-    height: auto;
-    width: 100%;
-
-    a{
-        
-    }
-  
-`
-export const TopBottomWrapper = styled.div`
-
-    position: relative;
-    margin: auto;
-  
-`
-
