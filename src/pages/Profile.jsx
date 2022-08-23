0d667c12a5d38333d35d8155e675bae210ad90df
Seb Amos <sebastian.amos8@gmail.com>
2022-08-23 14:29:28 +0100

    individual pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import UpdateUser from "../components/14-edit-user";
 import ListUsers from "../components/12-list-users";
 import LoginLogout from "../components/1-login-logout";
 import {useState, useEffect} from 'react';
-import Switch from "../components/17-switch";
+
 import UserProfile from "../components/13-user-profile";
 
 const Profile = ({user}) => {
