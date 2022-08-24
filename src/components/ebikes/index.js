89ef1db991dbfa2a30c5b69dc208a37318afb872
AmaniMagdoubi <amanimagdoubi@gmail.com>
2022-08-24 10:50:51 +0100

    deleted e-bike index.js / index.css and web and testing files

--- src/components/ebikes/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import EbikeApp from './ebikeApp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <EbikeApp />
-);
